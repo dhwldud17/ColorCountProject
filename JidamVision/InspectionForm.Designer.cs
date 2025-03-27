4e8bf011a655426d70970c26a07763cf8d2083f7
surrnder <ttl79369@naver.com>
2025-03-27 14:25:47 +0900

    불러오기 추가 필요

--- JidamVision/InspectionForm.Designer.cs
+++ JidamVision/InspectionForm.Designer.cs
@@ -43,17 +43,16 @@
             this.lbFaulty = new System.Windows.Forms.Label();
             this.lbPercent = new System.Windows.Forms.Label();
             this.lbTotalnumber = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageViewCCtrl1 = new JidamVision.ImageViewCCtrl();
+            this.btImageLode = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvMetric)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // bntStart
             // 
-            this.bntStart.Location = new System.Drawing.Point(391, 75);
-            this.bntStart.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bntStart.Location = new System.Drawing.Point(559, 112);
             this.bntStart.Name = "bntStart";
-            this.bntStart.Size = new System.Drawing.Size(129, 47);
+            this.bntStart.Size = new System.Drawing.Size(184, 50);
             this.bntStart.TabIndex = 1;
             this.bntStart.Text = "시작";
             this.bntStart.UseVisualStyleBackColor = true;
@@ -61,10 +60,9 @@
             // 
             // bntStop
             // 
-            this.bntStop.Location = new System.Drawing.Point(391, 151);
-            this.bntStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bntStop.Location = new System.Drawing.Point(558, 168);
             this.bntStop.Name = "bntStop";
-            this.bntStop.Size = new System.Drawing.Size(129, 47);
+            this.bntStop.Size = new System.Drawing.Size(184, 57);
             this.bntStop.TabIndex = 2;
             this.bntStop.Text = "정지";
             this.bntStop.UseVisualStyleBackColor = true;
@@ -72,141 +70,141 @@
             // 
             // rtbTotalnumber
             // 
-            this.rtbTotalnumber.Location = new System.Drawing.Point(391, 225);
-            this.rtbTotalnumber.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtbTotalnumber.Location = new System.Drawing.Point(559, 338);
             this.rtbTotalnumber.Name = "rtbTotalnumber";
-            this.rtbTotalnumber.Size = new System.Drawing.Size(130, 103);
+            this.rtbTotalnumber.Size = new System.Drawing.Size(184, 152);
             this.rtbTotalnumber.TabIndex = 3;
             this.rtbTotalnumber.Text = "";
             // 
             // rtbGood
             // 
-            this.rtbGood.Location = new System.Drawing.Point(391, 355);
-            this.rtbGood.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtbGood.Location = new System.Drawing.Point(559, 532);
             this.rtbGood.Name = "rtbGood";
-            this.rtbGood.Size = new System.Drawing.Size(61, 53);
+            this.rtbGood.Size = new System.Drawing.Size(85, 78);
             this.rtbGood.TabIndex = 4;
             this.rtbGood.Text = "";
             // 
             // rtbFaulty
             // 
-            this.rtbFaulty.Location = new System.Drawing.Point(461, 355);
-            this.rtbFaulty.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtbFaulty.Location = new System.Drawing.Point(659, 532);
             this.rtbFaulty.Name = "rtbFaulty";
-            this.rtbFaulty.Size = new System.Drawing.Size(60, 53);
+            this.rtbFaulty.Size = new System.Drawing.Size(84, 78);
             this.rtbFaulty.TabIndex = 5;
             this.rtbFaulty.Text = "";
             // 
             // rtbPercent
             // 
-            this.rtbPercent.Location = new System.Drawing.Point(533, 355);
-            this.rtbPercent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtbPercent.Location = new System.Drawing.Point(761, 532);
             this.rtbPercent.Name = "rtbPercent";
-            this.rtbPercent.Size = new System.Drawing.Size(129, 53);
+            this.rtbPercent.Size = new System.Drawing.Size(183, 78);
             this.rtbPercent.TabIndex = 6;
             this.rtbPercent.Text = "";
             // 
             // dtpStartTime
             // 
-            this.dtpStartTime.Location = new System.Drawing.Point(19, 44);
-            this.dtpStartTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpStartTime.Location = new System.Drawing.Point(27, 66);
             this.dtpStartTime.Name = "dtpStartTime";
-            this.dtpStartTime.Size = new System.Drawing.Size(236, 21);
+            this.dtpStartTime.Size = new System.Drawing.Size(335, 28);
             this.dtpStartTime.TabIndex = 7;
             // 
             // dgvMetric
             // 
             this.dgvMetric.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMetric.Location = new System.Drawing.Point(533, 44);
-            this.dgvMetric.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvMetric.Location = new System.Drawing.Point(761, 66);
             this.dgvMetric.Name = "dgvMetric";
             this.dgvMetric.RowHeadersWidth = 62;
             this.dgvMetric.RowTemplate.Height = 30;
-            this.dgvMetric.Size = new System.Drawing.Size(128, 283);
+            this.dgvMetric.Size = new System.Drawing.Size(183, 424);
             this.dgvMetric.TabIndex = 8;
             // 
             // dtpCurrenttime
             // 
-            this.dtpCurrenttime.Location = new System.Drawing.Point(285, 44);
-            this.dtpCurrenttime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpCurrenttime.Location = new System.Drawing.Point(407, 66);
             this.dtpCurrenttime.Name = "dtpCurrenttime";
-            this.dtpCurrenttime.Size = new System.Drawing.Size(236, 21);
+            this.dtpCurrenttime.Size = new System.Drawing.Size(335, 28);
             this.dtpCurrenttime.TabIndex = 9;
             // 
             // lbStartTime
             // 
             this.lbStartTime.AutoSize = true;
-            this.lbStartTime.Location = new System.Drawing.Point(17, 30);
-            this.lbStartTime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbStartTime.Location = new System.Drawing.Point(24, 45);
             this.lbStartTime.Name = "lbStartTime";
-            this.lbStartTime.Size = new System.Drawing.Size(53, 12);
+            this.lbStartTime.Size = new System.Drawing.Size(80, 18);
             this.lbStartTime.TabIndex = 10;
             this.lbStartTime.Text = "시작시간";
             // 
             // lbCurrenttime
             // 
             this.lbCurrenttime.AutoSize = true;
-            this.lbCurrenttime.Location = new System.Drawing.Point(283, 30);
-            this.lbCurrenttime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbCurrenttime.Location = new System.Drawing.Point(404, 45);
             this.lbCurrenttime.Name = "lbCurrenttime";
-            this.lbCurrenttime.Size = new System.Drawing.Size(53, 12);
+            this.lbCurrenttime.Size = new System.Drawing.Size(80, 18);
             this.lbCurrenttime.TabIndex = 11;
             this.lbCurrenttime.Text = "현재시간";
             // 
             // lbGood
             // 
             this.lbGood.AutoSize = true;
-            this.lbGood.Location = new System.Drawing.Point(391, 341);
-            this.lbGood.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbGood.Location = new System.Drawing.Point(559, 512);
             this.lbGood.Name = "lbGood";
-            this.lbGood.Size = new System.Drawing.Size(29, 12);
+            this.lbGood.Size = new System.Drawing.Size(44, 18);
             this.lbGood.TabIndex = 12;
             this.lbGood.Text = "양품";
             // 
             // lbFaulty
             // 
             this.lbFaulty.AutoSize = true;
-            this.lbFaulty.Location = new System.Drawing.Point(459, 341);
-            this.lbFaulty.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbFaulty.Location = new System.Drawing.Point(656, 512);
             this.lbFaulty.Name = "lbFaulty";
-            this.lbFaulty.Size = new System.Drawing.Size(29, 12);
+            this.lbFaulty.Size = new System.Drawing.Size(44, 18);
             this.lbFaulty.TabIndex = 13;
             this.lbFaulty.Text = "불량";
             // 
             // lbPercent
             // 
             this.lbPercent.AutoSize = true;
-            this.lbPercent.Location = new System.Drawing.Point(531, 341);
-            this.lbPercent.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbPercent.Location = new System.Drawing.Point(758, 511);
             this.lbPercent.Name = "lbPercent";
-            this.lbPercent.Size = new System.Drawing.Size(41, 12);
+            this.lbPercent.Size = new System.Drawing.Size(62, 18);
             this.lbPercent.TabIndex = 14;
             this.lbPercent.Text = "퍼센트";
             // 
             // lbTotalnumber
             // 
             this.lbTotalnumber.AutoSize = true;
-            this.lbTotalnumber.Location = new System.Drawing.Point(391, 211);
-            this.lbTotalnumber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbTotalnumber.Location = new System.Drawing.Point(559, 316);
             this.lbTotalnumber.Name = "lbTotalnumber";
-            this.lbTotalnumber.Size = new System.Drawing.Size(41, 12);
+            this.lbTotalnumber.Size = new System.Drawing.Size(62, 18);
             this.lbTotalnumber.TabIndex = 15;
             this.lbTotalnumber.Text = "총개수";
             // 
-            // pictureBox1
+            // imageViewCCtrl1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(19, 76);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(368, 332);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.imageViewCCtrl1.AutoSize = true;
+            this.imageViewCCtrl1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.imageViewCCtrl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageViewCCtrl1.Location = new System.Drawing.Point(27, 112);
+            this.imageViewCCtrl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.imageViewCCtrl1.Name = "imageViewCCtrl1";
+            this.imageViewCCtrl1.Size = new System.Drawing.Size(506, 482);
+            this.imageViewCCtrl1.TabIndex = 16;
+            // 
+            // btImageLode
+            // 
+            this.btImageLode.Location = new System.Drawing.Point(559, 231);
+            this.btImageLode.Name = "btImageLode";
+            this.btImageLode.Size = new System.Drawing.Size(181, 57);
+            this.btImageLode.TabIndex = 17;
+            this.btImageLode.Text = "불러오기";
+            this.btImageLode.UseVisualStyleBackColor = true;
             // 
             // InspectionForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 412);
+            this.ClientSize = new System.Drawing.Size(954, 618);
+            this.Controls.Add(this.btImageLode);
+            this.Controls.Add(this.imageViewCCtrl1);
             this.Controls.Add(this.lbTotalnumber);
             this.Controls.Add(this.lbPercent);
             this.Controls.Add(this.lbFaulty);
@@ -222,13 +220,10 @@
             this.Controls.Add(this.rtbTotalnumber);
             this.Controls.Add(this.bntStop);
             this.Controls.Add(this.bntStart);
-            this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "InspectionForm";
             this.Text = "InspectionForm";
             this.Resize += new System.EventHandler(this.InspectionForm_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.dgvMetric)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -250,6 +245,7 @@
         private System.Windows.Forms.Label lbFaulty;
         private System.Windows.Forms.Label lbPercent;
         private System.Windows.Forms.Label lbTotalnumber;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private ImageViewCCtrl imageViewCCtrl1;
+        private System.Windows.Forms.Button btImageLode;
     }
 }
